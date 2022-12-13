1dbee4209360643f9ca0d789b21a66e580cf15f0
Omer Hadash <hadashom@post.bgu.ac.il>
2022-12-13 11:09:42 +0200

    second commit

---
+++ static/JS/DashboardJS.js
@@ -0,0 +1,11 @@
+
+let currentPathWithoutPageName = getCurrentPathWithoutPageName();
+
+const logOutBtn = document.querySelector('button.log-out');
+const sliderBar = document.getElementById('myRange');
+
+sliderBar.disabled=true;             //First make sure it is not disabled
+
+logOutBtn.addEventListener('click', () => {
+    nextPageFun(currentPathWithoutPageName + "/Home.html");
+})
