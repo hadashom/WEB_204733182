4dab847a8ad432db9a00a01c7a16c68a7708987d
hadashom <hadashom@post.bgu.ac.il>
2023-01-17 13:00:18 +0200

    added things

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 8080;
+app.use(express.static('static'));
 
-//modules
-var express = require('express');
-var app = express();
-var path = require('path');
-const port = 8000;
-app.use(exoress.static('static'));
-
-//routes
 app.get('/', (req, res) => {
-  //res.send('Hello World Express!');
-  res.sendFile(path.join(__dirname, "All Code/views/Home.html"))
-});
+    res.redirect("/Home");
+})
 
+app.get('/Home', (req, res) => {
+    res.sendFile(path.join(__dirname, "views/Home.html"));
+})
 
-//listen
 app.listen(port, () => {
-  console.log("app is running on port " + port);
+    console.log("server is running on port " + port);
 })
